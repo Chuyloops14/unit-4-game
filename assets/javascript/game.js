09568f63b45144a2d3f3cf53f1b5e0a2ed922326
chuyloops <alpizarjesus84@gmail.com>
2019-05-18 21:35:06 -0700

    ds store

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    var Random= Math.floor(Math.radnom()*101+19)
+    var Random=Math.floor(Math.radnom()*101+19)
 
     $('#randomNumber').text(Random);
 
-    var num1 = Math.floor(Math.random()*11+1)
-    var num2 = Math.floor(Math.random()*11+1)
-    var num3 = Math.floor(Math.random()*11+1)
-    var num4 = Math.floor(Math.random()*11+1)
+    var num1= Math.floor(Math.random()*11+1)
+    var num2= Math.floor(Math.random()*11+1)
+    var num3= Math.floor(Math.random()*11+1)
+    var num4= Math.floor(Math.random()*11+1)
 
     var userTotal= 0;
     var wins= 0;
