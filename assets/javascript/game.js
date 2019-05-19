edd1cfc0600959612500d8b975cda7866fa986c3
chuyloops <alpizarjesus84@gmail.com>
2019-05-18 17:06:35 -0700

    added the java file and added code

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+$( documnent ).ready(function(){
+    var Random= Math.floor(Math.radnom()*101+19)
+
+    $('#randomNumber').text(Random);
+
+    var num1= Math.floor(Math.random()*11+1)
+    var num2= Math.floor(Math.random()*11+1)
+    var num3= Math.floor(Math.random()*11+1)
+    var num4= Math.floor(Math.random()*11+1)
+
+    var userTotal= 0;
+    var wins= 0;
+    var losses= 0;
+
+$('#numberWins=').text(wins);
+$('#nymberLosses').text(losses);
+
+function
+});
